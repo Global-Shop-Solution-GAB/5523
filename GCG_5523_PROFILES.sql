697a4823787650865cd92d25d5e3665c74b5c5b3
Service Web <sw@gssmail.com>
2024-03-14 11:28:20 -0500

    Commit changes in C:\Websites\Git\emarlow\5523\GCG_5523_PROFILES.sql

---
+++ GCG_5523_PROFILES.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_5523_PROFILES
+ * ========================================== */
+CREATE TABLE "GCG_5523_PROFILES"(
+ "ProfileName" CHAR(20),
+ "ScriptLocation" CHAR(200),
+ "RecordType" CHAR(1),
+ "GssUsername" CHAR(9),
+ "Terminal" CHAR(3),
+ "CSV_LocFile" CHAR(200),
+ "EDI_DestFolder" CHAR(200),
+ "ProjectToUse" CHAR(20));
+
+
