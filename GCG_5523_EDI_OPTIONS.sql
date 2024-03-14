e2a7c3693603fb320e3a3e8d420eef297c558c52
Service Web <sw@gssmail.com>
2024-03-14 11:28:20 -0500

    Commit changes in C:\Websites\Git\emarlow\5523\GCG_5523_EDI_OPTIONS.sql

---
+++ GCG_5523_EDI_OPTIONS.sql
@@ -0,0 +1,13 @@
+/* ==========================================
+ * TABLE: GCG_5523_EDI_OPTIONS
+ * ========================================== */
+CREATE TABLE "GCG_5523_EDI_OPTIONS"(
+ "FieldTitle" CHAR(200),
+ "FieldValue" CHAR(200),
+ "FieldType" CHAR(50),
+ "ProjectName" CHAR(20),
+ "UpdatedByUser" CHAR(9),
+ "OrderID" INTEGER,
+ "FieldTItleID" INTEGER);
+
+
